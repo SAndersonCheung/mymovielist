8f74cfb55a76bbb107ef515fc5ec9b5b91b2eea4
SAndersonCheung <summer.anderson99@hotmail.com>
2023-03-29 02:59:02 +0100

    Images for API Movie Posters
    - Loading movie posters from the API

--- mymovielist/src/components/AddMovie.js
+++ mymovielist/src/components/AddMovie.js
@@ -2,37 +2,50 @@ import Modal from "./Modal";
 import React, { useState } from "react";
 import MovieResult from "./MovieResult";
 import { useSearchMovie } from "../hooks/useSearchMovie";
+import styles from "../styles/components/Modal.module.scss";
 
 const AddMovie = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [searchString, setSearchString] = useState("");
 
   const moviesData = useSearchMovie({ searchString });
+
   return (
     <>
       <Modal isOpen={isModalOpen}>
-        <input
-          type="search"
-          name="Search"
-          onChange={(event) => {
-            setSearchString(event.target.value);
-          }}
-        ></input>
-        {moviesData?.results?.length > 0 &&
-          moviesData?.results?.map((movie) => (
-            <MovieResult
-              key={movie.id}
-              name={movie.title}
-              year={movie.release_date.substring(0, 4)}
-              rating={movie.vote_average}
-              imgUrl={movie.id}
-            />
-          ))}
+        <div className={styles.Modal_Topbar}>
+          <input
+            type="search"
+            name="Search"
+            placeholder="Search for movies here"
+            onChange={(event) => {
+              setSearchString(event.target.value);
+            }}
+          ></input>
+          <button
+            className={styles.Modal_Close}
+            onClick={() => setIsModalOpen(false)}
+          >
+            X
+          </button>
+        </div>
+        <div className={styles.Search_List}>
+          {moviesData?.results?.length > 0 &&
+            moviesData?.results?.map((movie) => (
+              <MovieResult
+                key={movie.id}
+                name={movie.title}
+                year={movie.release_date.substring(0, 4)}
+                rating={movie.vote_average}
+                imgUrl={"https://image.tmdb.org/t/p/w185" + movie.poster_path}
+              />
+            ))}
+        </div>
       </Modal>
       <input
         type="button"
-        value={isModalOpen ? "Close Movie Adding Modal" : "Add a new Movie"}
-        onClick={() => setIsModalOpen(!isModalOpen)}
+        value={"Add a new Movie"}
+        onClick={() => setIsModalOpen(true)}
       ></input>
     </>
   );
