dce4aca2181f4abad8f2abe271d411c878871594
SAndersonCheung <summer.anderson99@hotmail.com>
2023-03-29 02:59:20 +0100

    Express Install
    - Installing Express to link Backend Database to Frontend

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ export const movieSchema = new mongoose.Schema({
 export const Movie = new mongoose.model("Movie", movieSchema);
 
 const testMovie = new Movie({
-  movieName: "cool movie!",
-  movieRating: 98,
+  movieName: "The Whale",
+  movieRating: 8.045,
   movieYear: 2022,
   movieImg: "link",
   movieStatus: "To Watch",
@@ -20,8 +20,8 @@ const testMovie = new Movie({
 
 const main = async () => {
   await mongoose.connect("mongodb://127.0.0.1:27017/mylist");
-  // testMovie.save();
-  await Movie.deleteMany({}, console.log("done"));
+  testMovie.save();
+  // await Movie.deleteMany({}, console.log("done"));
   Movie.find({}).then((res) => console.log(res));
 };
 
